2d917714e23f3422574da4bf5faeaabf6d0be9a7
Kris Kemper <kriskemper@gmail.com>
2010-03-23 00:21:27 -0400

    Cleaned up some more test names so that they read well with the shoulda syntax.

--- test/functional/models/playlist_entry_test.rb
+++ test/functional/models/playlist_entry_test.rb
@@ -1,76 +1,90 @@
 require File.expand_path(File.dirname(__FILE__) + "/../functional_test_helper")
 
 class PlaylistEntryTest < Test::Unit::TestCase
-
-  should "find entries ready to play" do
-    PlaylistEntry.create! :file_location => 'first', :status => PlaylistEntry::UNPLAYED
-    PlaylistEntry.create! :file_location => 'second', :status => PlaylistEntry::UNPLAYED
+  
+  context "find_all_ready_to_play" do
+    should "find entries ready to play" do
+      PlaylistEntry.create! :file_location => 'first', :status => PlaylistEntry::UNPLAYED
+      PlaylistEntry.create! :file_location => 'second', :status => PlaylistEntry::UNPLAYED
     
-    assert_equal 'first', PlaylistEntry.find_all_ready_to_play.first.file_location
-  end
-
-  should "skip request should create a new skip request" do
-    entry = PlaylistEntry.create! :file_location => 'some_location', :status => PlaylistEntry::PLAYING
-    PlaylistEntry.request_skip("127.0.0.1", entry.id)
-    request = SkipRequest.find(:first)
-    assert_equal 'some_location', request.file_location
-    assert_equal '127.0.0.1', request.requested_by
-  end
-
-  should "skip requests returns all the skip requests" do
-    entry = PlaylistEntry.create! :file_location => 'some_location', :status => PlaylistEntry::UNPLAYED
-    skip_request1 = SkipRequest.create! :requested_by => "127.0.0.1", :file_location => 'some_location'
-    skip_request2 = SkipRequest.create! :requested_by => "127.0.0.2", :file_location => 'some_location'
-    assert_equal 2, entry.skip_requests.size
-    assert_true entry.skip_requests.include?(skip_request1)
-    assert_true entry.skip_requests.include?(skip_request2)
-  end
-
-  should "can skip returns true if there are as many skip requests as the jukebox options" do
-    entry = PlaylistEntry.create! :file_location => 'some_location', :status => PlaylistEntry::UNPLAYED
-    JukeboxOptions::NumberOfRequestsInOrderToSkip.times do |count|
-      SkipRequest.create! :requested_by => "127.0.0.#{count}", :file_location => 'some_location'
+      assert_equal 'first', PlaylistEntry.find_all_ready_to_play.first.file_location
     end
-    assert_true entry.can_skip?
   end
 
-  should "can skip returns false if there are not many skip requests as the jukebox options" do
-    entry = PlaylistEntry.create! :file_location => 'some_location', :status => PlaylistEntry::UNPLAYED
-    (JukeboxOptions::NumberOfRequestsInOrderToSkip - 1).times do |count|
-      SkipRequest.create! :requested_by => "127.0.0.#{count}", :file_location => 'some_location'
+  context "request_skip" do
+    should "create a new skip request" do
+      entry = PlaylistEntry.create! :file_location => 'some_location', :status => PlaylistEntry::PLAYING
+      PlaylistEntry.request_skip("127.0.0.1", entry.id)
+      request = SkipRequest.find(:first)
+      assert_equal 'some_location', request.file_location
+      assert_equal '127.0.0.1', request.requested_by
     end
-    assert_false entry.can_skip?
-  end
-
-  should "file exists return true if the file exists" do
-    entry = PlaylistEntry.create! :file_location => Test::ExistingFileLocation, :status => PlaylistEntry::UNPLAYED
-    assert_true entry.file_exists?
-  end
-
-  should "file exists return false if the file does not exist" do
-    entry = PlaylistEntry.create! :file_location => Test::NonExistingFileLocation, :status => PlaylistEntry::UNPLAYED
-    assert_false entry.file_exists?
   end
   
-  should "destroy non existent entries" do
-    PlaylistEntry.create! :file_location => Test::NonExistingFileLocation, :status => PlaylistEntry::UNPLAYED
-    PlaylistEntry.destroy_non_existent_entries
-    assert_equal 0, PlaylistEntry.find(:all).size
-  end
-  
-  should "create random will not create a playlist entry that exists" do
-    all_mp3s = Dir.glob(File.join([JUKEBOX_MUSIC_ROOT, "**", "*.mp3"].compact))
-    all_mp3s.each do |file_location|
-      PlaylistEntry.create! :file_location => file_location, :status => PlaylistEntry::UNPLAYED
+  context "skip_requests" do
+    should "return all the skip requests" do
+      entry = PlaylistEntry.create! :file_location => 'some_location', :status => PlaylistEntry::UNPLAYED
+      skip_request1 = SkipRequest.create! :requested_by => "127.0.0.1", :file_location => 'some_location'
+      skip_request2 = SkipRequest.create! :requested_by => "127.0.0.2", :file_location => 'some_location'
+      assert_equal 2, entry.skip_requests.size
+      assert_true entry.skip_requests.include?(skip_request1)
+      assert_true entry.skip_requests.include?(skip_request2)
     end
-    PlaylistEntry.create_random!
-    assert_equal all_mp3s.size, PlaylistEntry.find(:all).size
   end
 
-  should "create random will not create a playlist entry that exists when playing more than the number of songs" do
-    all_mp3s = Dir.glob(File.join([JUKEBOX_MUSIC_ROOT, "**", "*.mp3"].compact))
-    PlaylistEntry.create_random! :number_to_create => all_mp3s.size + 1
-    assert_equal all_mp3s.size, PlaylistEntry.find(:all).size
+  context "can_skip" do
+    should "return true if there are as many skip requests as the jukebox options" do
+      entry = PlaylistEntry.create! :file_location => 'some_location', :status => PlaylistEntry::UNPLAYED
+      JukeboxOptions::NumberOfRequestsInOrderToSkip.times do |count|
+        SkipRequest.create! :requested_by => "127.0.0.#{count}", :file_location => 'some_location'
+      end
+      assert_true entry.can_skip?
+    end
+
+    should "return false if there are not many skip requests as the jukebox options" do
+      entry = PlaylistEntry.create! :file_location => 'some_location', :status => PlaylistEntry::UNPLAYED
+      (JukeboxOptions::NumberOfRequestsInOrderToSkip - 1).times do |count|
+        SkipRequest.create! :requested_by => "127.0.0.#{count}", :file_location => 'some_location'
+      end
+      assert_false entry.can_skip?
+    end
+  end
+
+  context "file_exists" do
+    should "return true if the file exists" do
+      entry = PlaylistEntry.create! :file_location => Test::ExistingFileLocation, :status => PlaylistEntry::UNPLAYED
+      assert_true entry.file_exists?
+    end
+
+    should "return false if the file does not exist" do
+      entry = PlaylistEntry.create! :file_location => Test::NonExistingFileLocation, :status => PlaylistEntry::UNPLAYED
+      assert_false entry.file_exists?
+    end
   end
   
+  context "destroy non existent entries" do
+    should "destroy entries that have file locations that don't point to a file" do
+      PlaylistEntry.create! :file_location => Test::NonExistingFileLocation, :status => PlaylistEntry::UNPLAYED
+      PlaylistEntry.destroy_non_existent_entries
+      assert_equal 0, PlaylistEntry.find(:all).size
+    end
+  end
+  
+  context "create_random" do
+    should "not create a playlist entry that exists" do
+      all_mp3s = Dir.glob(File.join([JUKEBOX_MUSIC_ROOT, "**", "*.mp3"].compact))
+      all_mp3s.each do |file_location|
+        PlaylistEntry.create! :file_location => file_location, :status => PlaylistEntry::UNPLAYED
+      end
+      PlaylistEntry.create_random!
+      assert_equal all_mp3s.size, PlaylistEntry.find(:all).size
+    end
+
+    should "not create a playlist entry that exists when playing more than the number of songs" do
+      all_mp3s = Dir.glob(File.join([JUKEBOX_MUSIC_ROOT, "**", "*.mp3"].compact))
+      PlaylistEntry.create_random! :number_to_create => all_mp3s.size + 1
+      assert_equal all_mp3s.size, PlaylistEntry.find(:all).size
+    end
+  end
+
 end
--- test/functional/models/skip_request_test.rb
+++ test/functional/models/skip_request_test.rb
@@ -2,58 +2,59 @@ require File.expand_path(File.dirname(__FILE__) + "/../functional_test_helper")
 
 class SkipRequestTest < Test::Unit::TestCase
 
-  should "consider should create a new skip request when there are none" do
-    entry = PlaylistEntry.create! :file_location => 'some_location', :status => PlaylistEntry::PLAYING
-    SkipRequest.consider "127.0.0.1", entry
-    request = SkipRequest.find(:first)
-    assert_equal 1, SkipRequest.count
-    assert_equal 'some_location', request.file_location
-    assert_equal '127.0.0.1', request.requested_by
-  end
-
-  should "consider should not create a new skip request when there is one from same day and ip address" do
-    entry = PlaylistEntry.create! :file_location => 'some_location', :status => PlaylistEntry::PLAYING
-    Time.is(Time.parse("Sun Mar 14 12:00:00")) do
-      SkipRequest.create! :requested_by => "127.0.0.1", :file_location => 'some_location'
-    end
-    Time.is(Time.parse("Sun Mar 14 12:05:00")) do
+  context "consider" do
+    should "create a new skip request when there are none" do
+      entry = PlaylistEntry.create! :file_location => 'some_location', :status => PlaylistEntry::PLAYING
       SkipRequest.consider "127.0.0.1", entry
+      request = SkipRequest.find(:first)
+      assert_equal 1, SkipRequest.count
+      assert_equal 'some_location', request.file_location
+      assert_equal '127.0.0.1', request.requested_by
     end
-    assert_equal 1, SkipRequest.count
-  end
 
-  should "consider should create a new skip request when there is one from the same day but different ip address" do
-    entry = PlaylistEntry.create! :file_location => 'some_location', :status => PlaylistEntry::PLAYING
-    Time.is(Time.parse("Sun Mar 14 12:00:00")) do
-      SkipRequest.create! :requested_by => "192.168.0.5", :file_location => 'some_location'
+    should "not create a new skip request when there is one from same day and ip address" do
+      entry = PlaylistEntry.create! :file_location => 'some_location', :status => PlaylistEntry::PLAYING
+      Time.is(Time.parse("Sun Mar 14 12:00:00")) do
+        SkipRequest.create! :requested_by => "127.0.0.1", :file_location => 'some_location'
+      end
+      Time.is(Time.parse("Sun Mar 14 12:05:00")) do
+        SkipRequest.consider "127.0.0.1", entry
+      end
+      assert_equal 1, SkipRequest.count
     end
-    Time.is(Time.parse("Sun Mar 14 12:05:00")) do
-      SkipRequest.consider "127.0.0.1", entry
-    end
-    assert_equal 2, SkipRequest.count
-  end
 
-  should "consider should create a new skip request when there is one from the same day and same ip address but different songs" do
-    entry = PlaylistEntry.create! :file_location => 'some_location', :status => PlaylistEntry::PLAYING
-    Time.is(Time.parse("Sun Mar 14 12:00:00")) do
-      SkipRequest.create! :requested_by => "127.0.0.1", :file_location => 'some_other_location'
+    should "create a new skip request when there is one from the same day but different ip address" do
+      entry = PlaylistEntry.create! :file_location => 'some_location', :status => PlaylistEntry::PLAYING
+      Time.is(Time.parse("Sun Mar 14 12:00:00")) do
+        SkipRequest.create! :requested_by => "192.168.0.5", :file_location => 'some_location'
+      end
+      Time.is(Time.parse("Sun Mar 14 12:05:00")) do
+        SkipRequest.consider "127.0.0.1", entry
+      end
+      assert_equal 2, SkipRequest.count
     end
-    Time.is(Time.parse("Sun Mar 14 12:05:00")) do
-      SkipRequest.consider "127.0.0.1", entry
+
+    should "create a new skip request when there is one from the same day and same ip address but different songs" do
+      entry = PlaylistEntry.create! :file_location => 'some_location', :status => PlaylistEntry::PLAYING
+      Time.is(Time.parse("Sun Mar 14 12:00:00")) do
+        SkipRequest.create! :requested_by => "127.0.0.1", :file_location => 'some_other_location'
+      end
+      Time.is(Time.parse("Sun Mar 14 12:05:00")) do
+        SkipRequest.consider "127.0.0.1", entry
+      end
+      assert_equal 2, SkipRequest.count
     end
-    assert_equal 2, SkipRequest.count
-  end
   
-  should "consider should purge old skip requests that are not from today and create a new skip request" do
-    entry = PlaylistEntry.create! :file_location => 'some_location', :status => PlaylistEntry::PLAYING
-    Time.is(Time.parse("Sun Mar 14 12:00:00")) do
-      SkipRequest.create! :requested_by => "127.0.0.1", :file_location => 'some_other_location'
+    should "purge old skip requests that are not from today and create a new skip request" do
+      entry = PlaylistEntry.create! :file_location => 'some_location', :status => PlaylistEntry::PLAYING
+      Time.is(Time.parse("Sun Mar 14 12:00:00")) do
+        SkipRequest.create! :requested_by => "127.0.0.1", :file_location => 'some_other_location'
+      end
+      Time.is(Time.parse("Sun Mar 15 12:05:00")) do
+        SkipRequest.consider "127.0.0.1", entry
+      end
+      assert_equal 1, SkipRequest.count
+      assert_equal Time.parse("Sun Mar 15 12:05:00").day, SkipRequest.find(:first).created_at.day
     end
-    Time.is(Time.parse("Sun Mar 15 12:05:00")) do
-      SkipRequest.consider "127.0.0.1", entry
-    end
-    assert_equal 1, SkipRequest.count
-    assert_equal Time.parse("Sun Mar 15 12:05:00").day, SkipRequest.find(:first).created_at.day
   end
-  
 end
